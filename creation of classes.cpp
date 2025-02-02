62c46e10d9f9b6b28e6a19be5ff1d41e372434b2
Arnold-Peter <noddyjr000@gmail.com>
2025-02-02 15:29:57 +0300

    Create creation of classes.cpp

---
+++ creation of classes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// attributes (data menmbers)
+class student{
+	string name;
+	int age;
+	
+	// members function
+	void diplayDetails(){
+		cout<<"name:"<<name<<"\nage:"<<age<<endl;
+		
+	}
+};
+
+int student(){
+   //creating an object of the class
+   student;
+	student.name: = "John Doe";
+	student.age = "20";
+	
+	student.displayDetails();
+	return 0;
+	
+}
